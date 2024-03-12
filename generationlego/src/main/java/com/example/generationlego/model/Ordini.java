617eb943c8ff6083da5501cf64e8f19db7715303
DanielePonticella <danieleponticella@outlook.com>
2024-03-12 12:17:45 +0100

    aggiunta utenti

--- generationlego/src/main/java/com/example/generationlego/model/Ordini.java
+++ generationlego/src/main/java/com/example/generationlego/model/Ordini.java
@@ -1,4 +1,18 @@
 package com.example.generationlego.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="ordini")
 public class Ordini {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private Date data;
+    @Column
+    private Double importo;
+    @Column
 }
--- generationlego/src/main/java/com/example/generationlego/model/Utenti.java
+++ generationlego/src/main/java/com/example/generationlego/model/Utenti.java
@@ -1,4 +1,141 @@
 package com.example.generationlego.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="utenti")
 public class Utenti {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String cognome;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String via;
+    @Column
+    private String citta;
+    @Column
+    private String provincia;
+    @Column
+    private String cap;
+    @Column
+    private String mail;
+    @Column
+    private String telefono;
+    @Column
+    private Date data;
+    @Column
+    private String profilo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCap() {
+        return cap;
+    }
+
+    public void setCap(String cap) {
+        this.cap = cap;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getProfilo() {
+        return profilo;
+    }
+
+    public void setProfilo(String profilo) {
+        this.profilo = profilo;
+    }
 }
