3638a50faa80ee11534908ab676b90b7e27e9ee5
DanielePonticella <danieleponticella@outlook.com>
2024-03-12 12:31:23 +0100

    modifica classe Ordini

--- generationlego/src/main/java/com/example/generationlego/model/Ordini.java
+++ generationlego/src/main/java/com/example/generationlego/model/Ordini.java
@@ -2,6 +2,7 @@ package com.example.generationlego.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -11,8 +12,42 @@ public class Ordini {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
-    private Date data;
+    private LocalDate data;
     @Column
-    private Double importo;
-    @Column
+    private double importo;
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name="id_utente",referencedColumnName = "id")
+    private  Utenti utenti ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double getImporto() {
+        return importo;
+    }
+
+    public void setImporto(double importo) {
+        this.importo = importo;
+    }
+
+    public Utenti getUtenti() {
+        return utenti;
+    }
+
+    public void setUtenti(Utenti utenti) {
+        this.utenti = utenti;
+    }
 }
