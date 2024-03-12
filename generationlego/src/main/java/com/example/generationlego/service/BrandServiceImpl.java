e342255f730c866180c50eba24ba5874cbb790d3
DanielePonticella <danieleponticella@outlook.com>
2024-03-12 14:43:01 +0100

    Modifica Brandservice,Brandserviceimpl

--- generationlego/src/main/java/com/example/generationlego/service/BrandServiceImpl.java
+++ generationlego/src/main/java/com/example/generationlego/service/BrandServiceImpl.java
@@ -1,4 +1,33 @@
 package com.example.generationlego.service;
 
-public class BrandServiceImpl {
+import com.example.generationlego.dao.BrandDao;
+import com.example.generationlego.model.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BrandServiceImpl implements BrandService{
+    @Autowired
+    private BrandDao brandDao;
+    @Override
+    public void registraBrand(Brand brand) {
+        brandDao.save(brand);
+
+    }
+
+    @Override
+    public List<Brand> getBrand() {
+        return (List<Brand>) brandDao.findAll();
+    }
+
+    @Override
+    public Brand getBrandById(int id) {
+        return brandDao.findById(id).get();
+    }
+
+    @Override
+    public void cancellaBrand(int id) {
+        brandDao.deleteById(id);
+
+    }
 }
