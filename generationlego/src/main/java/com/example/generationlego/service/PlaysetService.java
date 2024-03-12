841125872bd210b0b7a53ae3e0290299c686f8e7
Mamels4 <paolomameli987@gmail.com>
2024-03-12 15:11:39 +0100

    Update PlaysetService e Impl

--- generationlego/src/main/java/com/example/generationlego/service/PlaysetService.java
+++ generationlego/src/main/java/com/example/generationlego/service/PlaysetService.java
@@ -1,4 +1,23 @@
 package com.example.generationlego.service;
 
-public interface PlaysetService {
+import com.example.generationlego.model.Playset;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface PlaysetService
+{
+    List<Playset> getPlayset();
+    Playset getPlaysetById(int id);
+    boolean aggiungiAlCarrello(int id, HttpSession session);
+    List<Playset> getCarrello(HttpSession session);
+    void rimuoviDalCarrello(int id, HttpSession session);
+    double getTotaleCarrello(HttpSession session);
+    void registraPlayset(Playset playset, String nome, int id_brand, String eta, int numero_pezzi,
+                         float prezzo, String descrizione, MultipartFile immagine, int scorte);
+    void cancellaPlayset(int id);
+
+    Object validaPlayset(Playset playset, String nome, int id_brand, String eta, int numero_pezzi,
+                         float prezzo, String descrizione);
 }
--- generationlego/src/main/java/com/example/generationlego/service/PlaysetServiceImpl.java
+++ generationlego/src/main/java/com/example/generationlego/service/PlaysetServiceImpl.java
@@ -1,4 +1,141 @@
 package com.example.generationlego.service;
 
-public class PlaysetServiceImpl {
+import com.example.generationlego.dao.PlaysetDao;
+import com.example.generationlego.model.Playset;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class PlaysetServiceImpl implements PlaysetService{
+
+    @Autowired
+    private PlaysetDao playsetDao;
+    @Autowired
+    private BrandService brandService;
+    @Override
+    public List<Playset> getPlayset() {
+        List<Playset> playset = (List<Playset>) playsetDao.findAll();
+        return playset;
+    }
+
+    @Override
+    public Playset getPlaysetById(int id) {
+        Optional<Playset> playsetOptional = playsetDao.findById(id);
+        if(playsetOptional.isPresent())
+            return playsetOptional.get();
+        return null;
+    }
+
+    @Override
+    public boolean aggiungiAlCarrello(int id, HttpSession session) {
+        Playset playset = getPlaysetById(id);
+        List<Playset> carrello;
+        if(session.getAttribute("carrello") != null)
+        {
+            carrello = (List<Playset>) session.getAttribute("carrello");
+            for(Playset p : carrello)
+                if(p.getId() == id)
+                    return false;
+            carrello.add(playset);
+            session.setAttribute("carrello", carrello);
+            return true;
+        } else {
+            carrello = new ArrayList<>();
+            carrello.add(playset);
+            session.setAttribute("carrello", carrello);
+            return true;
+        }
+    }
+
+    @Override
+    public List<Playset> getCarrello(HttpSession session) {
+        if(session.getAttribute("carrello") != null)
+            return (List<Playset>) session.getAttribute("carrello");
+        return null;
+    }
+
+    @Override
+    public void rimuoviDalCarrello(int id, HttpSession session) {
+        List<Playset> carrello = (List<Playset>) session.getAttribute("carrello");
+        for(Playset p : carrello)
+            if(p.getId() == id)
+            {
+                carrello.remove(p);
+                break;
+            }
+        carrello = carrello
+                .stream()
+                .filter(p -> p.getId() != id)
+                .collect(Collectors.toList());
+        if(carrello.size() > 0)
+            session.setAttribute("carrello", carrello);
+        else
+            session.removeAttribute("carrello");
+
+    }
+
+    @Override
+    public double getTotaleCarrello(HttpSession session) {
+        List<Playset> carrello = (List<Playset>) session.getAttribute("carrello");
+        if(carrello != null)
+            return carrello
+                    .stream()
+                    .mapToDouble(Playset::getPrezzo)
+                    .reduce(0.0, (p1,p2) -> p1 + p2);
+        return 0;
+    }
+
+    @Override
+    public void registraPlayset(Playset playset, String nome, int id_brand, String eta, int numero_pezzi,
+                                float prezzo, String descrizione, MultipartFile immagine, int scorte) {
+        playset.setNome(nome);
+        playset.setBrand(brandService.getBrandById(id_brand));
+        playset.setEta(eta);
+        playset.setNumero_pezzi(numero_pezzi);
+        playset.setPrezzo(prezzo);
+        playset.setDescrizione(descrizione);
+        if(immagine != null && !immagine.isEmpty())
+        {
+            try
+            {
+                String formato = immagine.getContentType();
+                String immagineCodificata = "data:" + formato + ";base64," +
+                        Base64.getEncoder().encodeToString(immagine.getBytes());
+                playset.setImmagine(immagineCodificata);
+            } catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        playsetDao.save(playset);
+    }
+
+    @Override
+    public void cancellaPlayset(int id) {
+        playsetDao.deleteById(id);
+    }
+
+    @Override
+    public Object validaPlayset(Playset playset, String nome, int id_brand,
+                                String eta, int numero_pezzi, float prezzo, String descrizione) {
+        playset.setBrand(brandService.getBrandById(id_brand));
+        playset.setNome(nome);
+        Map<String, String> errori = new HashMap<>();
+        if(!Pattern.matches("[a-zA-Z0-9\\sàèìòù,.-]{1,50}", nome))
+            errori.put("nome", "Caratteri non ammessi");
+        try {
+            playset.setPrezzo(prezzo);
+        } catch (Exception e)
+        {
+            errori.put("prezzo", "Il prezzo indicato non è corretto");
+        }
+        if(errori.size() > 0)
+            return new Object[]{playset,errori};
+        else
+            return null;
+    }
 }
