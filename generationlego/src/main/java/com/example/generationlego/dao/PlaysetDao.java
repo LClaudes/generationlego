a20fdcdee0db4cb5d8ae464d687b57c6acf07835
Mamels4 <paolomameli987@gmail.com>
2024-03-12 14:17:18 +0100

    Update PlaysetDao

--- generationlego/src/main/java/com/example/generationlego/dao/PlaysetDao.java
+++ generationlego/src/main/java/com/example/generationlego/dao/PlaysetDao.java
@@ -1,6 +1,7 @@
 package com.example.generationlego.dao;
 
+import com.example.generationlego.model.Playset;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PlaysetDao extends CrudRepository{
+public interface PlaysetDao extends CrudRepository<Playset, Integer>{
 }
