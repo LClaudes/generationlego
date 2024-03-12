452a5afd6b7a0fb8ae8b22d0dc29dc3e58aa0ecb
DanielePonticella <danieleponticella@outlook.com>
2024-03-12 15:30:25 +0100

    Creo IndexController

---
+++ generationlego/src/main/java/com/example/generationlego/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.example.generationlego.controller;
+
+import com.example.generationlego.model.Playset;
+import com.example.generationlego.service.PlaysetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @Autowired
+    private PlaysetService playsetService;
+    @GetMapping
+    public String getPage(Model model){
+        List<Playset> playset = playsetService.getPlayset();
+        model.addAttribute("playset",playset);
+        return "index";
+
+    }
+}
