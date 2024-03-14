9a18c516fecadb3d0af9605291c652c55e9f5a5a
AlessioPacioni97 <pacionialessio8@gmail.com>
2024-03-14 15:00:01 +0100

    index,catalogo,css

--- generationlego/src/main/resources/static/js/script.js
+++ generationlego/src/main/resources/static/js/script.js
@@ -1,14 +1,12 @@
 // script.js
 
 // Funzione per aggiungere il prodotto al carrello
-function aggiungiAlCarrello() {
+//function aggiungiAlCarrello() {
     // Mostra un alert quando il pulsante "Aggiungi al Carrello" viene premuto
-    alert("Prodotto aggiunto al carrello!");
-}
- function openNav() {
-      document.getElementById("mySidebar").style.width = "250px";
-    }
+    //alert("Prodotto aggiunto al carrello!");
+//}
+function openNav() {
+      document.getElementById("mySidebar").style.width = "250px"}
 
-    function closeNav() {
-      document.getElementById("mySidebar").style.width = "0";
-    }
+function closeNav() {
+      document.getElementById("mySidebar").style.width = "0"}
